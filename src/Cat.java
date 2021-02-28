6149421641308f94cf06950b20830bc7222485c8
Alexander <karvat808@yandex.ru>
2021-02-28 16:22:04 +0700

    added changes in Main.java and Cat.java (1)

--- src/Cat.java
+++ src/Cat.java
@@ -1,4 +1,10 @@
 public class Cat {
+  private String parent;
+
+  public Cat(String parent) {
+    this.parent = parent;
+  }
+
   public static void meow() {
     System.out.println("Meow!");
   }
